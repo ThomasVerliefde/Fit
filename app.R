9a06db4d9849124d5ff592bc89b92d5d425a9d40
Thomas <tverliefde@hotmail.com>
2018-09-19 13:48:18 +0200

    Add files via upload
    Version 1.1, with possibility to change sample sizes, for both training and test (old & new), with updated lay-out (boxed graphs), and larger y-margins

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 # Title: Fit
 # Author: Thomas Verliefde
-# Date: 2018/05/09
-# Version: 1.0
+# Date: 2018/09/19
+# Version: 1.1
 
 # Checks whether packages are installed, and installs the ones which are not.
 # It should not reinstall already installed packages.
@@ -19,11 +19,26 @@ shinyApp(
           'refresh','Resample',icon('refresh'))
       ),
       column(
-        8,
+        4,
         HTML('<h1> <div align="center"> Generalizability of Fit <div></h1>')
       ),
       column(
-        2
+        2,
+        numericInput(
+          "oldN","Initial Sample Size",15,min=15,max=100
+        )
+        ),
+      column(
+        2,
+        numericInput(
+          'newN','Retest Sample Size',15,min=15,max=100
+        )
+      ),
+      column(
+        2,
+        numericInput(
+          'polyD','# Degrees Polynomial',6,min=3,max=15
+        )
       )
     ),
     fluidRow(
@@ -39,52 +54,77 @@ shinyApp(
   
   server = function(input, output, session) {
     
-    
     trueDist = . %>% poly(2,raw=T,simple=T) %>% multiply_by_matrix(matrix(1,2))
     xmin = -2
     xmax = 2
-    ymargin = 2
-    n = 15
-    iter = 2
+    ymargin = 3
     ymin = seq(xmin,xmax,length.out=101) %>%
       trueDist(.) %>% min %>% subtract(ymargin) %>% floor
     ymax = seq(xmin,xmax,length.out=101) %>%
       trueDist(.) %>% max %>% add(ymargin) %>% ceiling
-    degrees = c(1,2,8)
+    degrees = reactive(c(1,2,input$polyD))
     
-    data = eventReactive(
+    data1 = eventReactive(
       input$refresh,
-      {replicate(
-        iter,
-        runif(n,xmin,xmax)
-      ) %>%
-          as_tibble %>%
-          rename_all(
-            funs(
-              gsub('V','x',.)
-            )
+      {
+        tibble(
+          x1 = runif(input$oldN,xmin,xmax)
           ) %>%
-          bind_cols(
-            transmute_all(
-              .,
-              funs(
-                trueDist(.) %>% add(rnorm(n))
-              )
-            ) %>%
-              rename_all(
-                funs(
-                  gsub('x','y',.)
-                )
-              )
-          )
-      },ignoreNULL = FALSE
+          mutate(
+          y1 = trueDist(x1) %>% add(rnorm(input$oldN))
+        )
+      },
+      ignoreNULL = FALSE
     )
+    
+    data2 = eventReactive(
+      input$refresh,
+      {
+        tibble(
+          x2 = runif(input$newN,xmin,xmax)
+          ) %>%
+          mutate(
+          y2 = trueDist(x2) %>% add(rnorm(input$newN))
+        )
+      },
+      ignoreNULL = FALSE
+    )
+        
+        
+        
+    #   data = eventReactive(
+    #     input$refresh,
+    #     {replicate(
+    #     iter,
+    #     runif(input$oldN,xmin,xmax)
+    #   ) %>%
+    #       as_tibble %>%
+    #       rename_all(
+    #         funs(
+    #           gsub('V','x',.)
+    #         )
+    #       ) %>%
+    #       bind_cols(
+    #         transmute_all(
+    #           .,
+    #           funs(
+    #             trueDist(.) %>% add(rnorm(input$oldN))
+    #           )
+    #         ) %>%
+    #           rename_all(
+    #             funs(
+    #               gsub('x','y',.)
+    #             )
+    #           )
+    #       )
+    #   },ignoreNULL = FALSE
+    # )
     
     models = reactive(
       lapply(
-        degrees,
+        degrees(),
         function(l) {
-          data() %$%
+          data1() %$%
             lm(y1 ~ poly(x1,l,raw=T))
         }
       )
@@ -99,7 +139,7 @@ shinyApp(
       lapply(
         1:3,
         function(l) {
-          data() %$%
+          data1() %$%
             Rsq(models()[[l]],y1,x1)
         }
       )
@@ -109,71 +149,142 @@ shinyApp(
       lapply(
         1:3,
         function(l) {
-          data() %$%
+          data2() %$%
             Rsq(models()[[l]],y2,x2)
         }
       )
     )
     
     p1 = reactive(
-      data() %>% ggplot(aes(x=x1,y=y1)) +
+      data1() %>% ggplot(aes(x=x1,y=y1)) +
         geom_point() +
         stat_function(fun = trueDist,col='grey') +
-        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees[1],raw=TRUE,coefs=NULL)) +
+        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees()[1],raw=TRUE,coefs=NULL)) +
         lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
-        labs(x = sprintf('R² = %.3f',Rsq1() %>% extract2(1)))
+        labs(x = sprintf('R² = %.3f',Rsq1() %>% extract2(1)),
+             y = 'old',
+             title = 'Linear') +
+        theme_light() +
+        theme(
+            axis.line = element_blank(),
+            axis.ticks = element_blank(),
+            axis.text = element_blank(),
+            panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank(),
+            panel.background = element_rect(
+              colour='black',fill=NA
+            )
+          )
     )
     
     p2 = reactive(
-      data() %>% ggplot(aes(x=x1,y=y1)) +
-      geom_point() +
-      stat_function(fun = function(x) {x+x^2},col='grey') +
-      stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees[2],raw=TRUE,coefs=NULL)) +
-      lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
-      labs(x = sprintf('R² = %.3f',Rsq1() %>% extract2(2)))
-    )
-
-    p3 = reactive(
-      data() %>% ggplot(aes(x=x1,y=y1)) +
-      geom_point() +
-      stat_function(fun = function(x) {x+x^2},col='grey') +
-      stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees[3],raw=TRUE,coefs=NULL)) +
-      lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
-      labs(x = sprintf('R² = %.3f',Rsq1() %>% extract2(3)))
-    )
-
-    p4 = reactive(
-      data() %>% ggplot(aes(x=x1,y=y1)) +
-      geom_point(aes(x=x2,y=y2)) +
-      stat_function(fun = trueDist,col='grey') +
-      stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees[1],raw=TRUE,coefs=NULL)) +
-      lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
-      labs(x = sprintf('R² = %.3f',Rsq2() %>% extract2(1)))
-    )
-
-    p5 = reactive(
-      data() %>% ggplot(aes(x=x1,y=y1)) +
-      geom_point(aes(x=x2,y=y2)) +
-      stat_function(fun = function(x) {x+x^2},col='grey') +
-      stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees[2],raw=TRUE,coefs=NULL)) +
-      lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
-      labs(x = sprintf('R² = %.3f',Rsq2() %>% extract2(2)))
-    )
-
-    p6 = reactive(
-      data() %>% ggplot(aes(x=x1,y=y1)) +
-      geom_point(aes(x=x2,y=y2)) +
-      stat_function(fun = function(x) {x+x^2},col='grey') +
-      stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees[3],raw=TRUE,coefs=NULL)) +
-      lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
-      labs(x = sprintf('R² = %.3f',Rsq2() %>% extract2(3)))
+      data1() %>% ggplot(aes(x=x1,y=y1)) +
+        geom_point() +
+        stat_function(fun = function(x) {x+x^2},col='grey') +
+        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees()[2],raw=TRUE,coefs=NULL)) +
+        lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
+        labs(x = sprintf('R² = %.3f',Rsq1() %>% extract2(2)),
+             y = '',
+             title = 'Quadratic') +
+        theme_light() +
+        theme(
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(
+            colour='black',fill=NA
+          )
+        )
     )
     
-    theme_update(
-      axis.line = element_blank(),
-      axis.ticks = element_blank(),
-      axis.text = element_blank(),
-      axis.title.y = element_blank()
+    p3 = reactive(
+      data1() %>% ggplot(aes(x=x1,y=y1)) +
+        geom_point() +
+        stat_function(fun = function(x) {x+x^2},col='grey') +
+        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees()[3],raw=TRUE,coefs=NULL)) +
+        lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
+        labs(x = sprintf('R² = %.3f',Rsq1() %>% extract2(3)),
+             y = '',
+             title = 'Polynomial') +
+        theme_light() +
+        theme(
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(
+            colour='black',fill=NA
+          )
+        )
+    )
+    
+    p4 = reactive(
+      data1() %>% ggplot(aes(x=x1,y=y1)) +
+        geom_point(data = data2(),aes(x=x2,y=y2)) +
+        stat_function(fun = trueDist,col='grey') +
+        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees()[1],raw=TRUE,coefs=NULL)) +
+        lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
+        labs(x = sprintf('R² = %.3f',Rsq2() %>% extract2(1)),
+             y = 'new',
+             title = '') +
+        theme_light() +
+        theme(
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(
+            colour='black',fill=NA
+          )
+        )
+    )
+    
+    p5 = reactive(
+      data1() %>% ggplot(aes(x=x1,y=y1)) +
+        geom_point(data = data2(),aes(x=x2,y=y2)) +
+        stat_function(fun = function(x) {x+x^2},col='grey') +
+        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees()[2],raw=TRUE,coefs=NULL)) +
+        lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
+        labs(x = sprintf('R² = %.3f',Rsq2() %>% extract2(2)),
+             y = '',
+             title = '') +
+        theme_light() +
+        theme(
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(
+            colour='black',fill=NA
+          )
+        )
+    )
+    
+    p6 = reactive(
+      data1() %>% ggplot(aes(x=x1,y=y1)) +
+        geom_point(data = data2(),aes(x=x2,y=y2)) +
+        stat_function(fun = function(x) {x+x^2},col='grey') +
+        stat_smooth(geom='line',method = "lm", formula = y ~ poly(x,degrees()[3],raw=TRUE,coefs=NULL)) +
+        lims(x = c(xmin,xmax), y = c(ymin,ymax)) +
+        labs(x = sprintf('R² = %.3f',Rsq2() %>% extract2(3)),
+             y = '',
+             title = '') +
+        theme_light() +
+        theme(
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(
+            colour='black',fill=NA
+          )
+        )
     )
     
     output$plot = renderPlot({
@@ -181,13 +292,7 @@ shinyApp(
         p1(),p2(),p3(),p4(),p5(),p6(),
         align = 'h',
         nrow = 2
-      ) +
-        draw_label('old', angle = 90, size = 13,x = 0.005,y = .75,fontface = 'bold') +
-        draw_label('new', angle = 90, size = 13,x = 0.005,y = .25,fontface = 'bold') +
-        draw_label('Linear',size = 14, x = .17, y = .99, fontface = 'bold') +
-        draw_label('Quadratic',size = 14, x = .5, y = .99, fontface = 'bold') +
-        draw_label(paste0(degrees[3],'-th Degree Polynomial'),size = 14, x = .84, y = .99, fontface = 'bold') +
-        theme(plot.margin = unit(c(.5,.5,.5,.5), "cm"))
+       )
     })
     
   }
